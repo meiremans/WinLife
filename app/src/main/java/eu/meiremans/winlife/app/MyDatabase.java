3e5beb1b60abdf46c6ae480f5acd151f9e5d07f3
meiremax <nickmeiremans@hotmail.com>
2014-11-19 01:02:03 +0100

    Fixed database issue

--- app/src/main/java/eu/meiremans/winlife/app/MyDatabase.java
+++ app/src/main/java/eu/meiremans/winlife/app/MyDatabase.java
@@ -8,7 +8,7 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
  */
 public class MyDatabase extends SQLiteAssetHelper {
 
-    private static final String DATABASE_NAME = "WinLife.db";
+    private static final String DATABASE_NAME = "WinLife";
     private static final int DATABASE_VERSION = 1;
 
     public MyDatabase(Context context) {
